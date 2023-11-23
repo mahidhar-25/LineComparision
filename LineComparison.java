320b4dc366c5a63e93eb24668b7b06d82d6c4659
mahidhar reddy <mahidhar100008@gmail.com>
2023-11-23 20:42:14 +0530

    Added : usecase 3 , both the lines are compared using compareTo methos and printing the which one is greater

--- LineComparison.java
+++ LineComparison.java
@@ -17,17 +17,35 @@ public class LineComparison {
         System.out.println("!! Welcome to Line Comparison !!");
         System.out.println("enter two points on a line to get distance : ");
         Scanner input = new Scanner(System.in);
+        //get the inputs of line 1
         int x1 = input.nextInt() , y1 = input.nextInt();
         int x2 = input.nextInt() , y2 = input.nextInt();
 
         double distanceLeftToRight = getDistanceBetweenPoints(x1 , y1 , x2 , y2);
-        double distanceRightToLeft = getDistanceBetweenPoints(x2 , y2 , x1 , y1);
+
+        //get the inputs of line 2
+        System.out.println("enter two points on a line to get distance : ");
+        int x3 = input.nextInt() , y3 = input.nextInt();
+        int x4 = input.nextInt() , y4 = input.nextInt();
+        double distanceRightToLeft = getDistanceBetweenPoints(x3 , y3 , x4 , y4);
+        //print the length of the two lines
         System.out.println("distance between two points (" + x1 + "," + y1 + ") and (" + x2 + "," + y2 + ") is " + distanceLeftToRight );
-        System.out.println("distance between two points (" + x2 + "," + y2 + ") and (" + x1 + "," + y1 + ") is " + distanceRightToLeft );
+        System.out.println("distance between two points (" + x3 + "," + y3 + ") and (" + x4 + "," + y4 + ") is " + distanceRightToLeft );
 
         //making double values as line objects , as equals method works on objects
-        Double line1 = Double.valueOf(distanceLeftToRight);
-        Double line2 = Double.valueOf(distanceLeftToRight);
-        System.out.println("Both the lines are " + (line1.equals(line2) ? "equal" : "not equal"));
+        Double line1 = distanceLeftToRight;
+        Double line2 = distanceRightToLeft;
+        //compare two lines using built in method
+        int comparedValue = line1.compareTo(line2);
+
+        // getting know more about lines , which is greater
+        if(comparedValue == 0){
+            System.out.println("Both the lines are equal");
+        }else if (comparedValue > 0){
+            System.out.println("line1 is greater than line2");
+        }else{
+            System.out.println("line2 is greater than line1");
+        }
+
     }
 }
